42f0bf23711627b46a8084d5b354b59a5fa860d3
Patrick Wang <patrickwang@Patricks-MacBook-Pro-3.local>
2025-06-21 15:46:36 -0500

    Initial commit: Configure Docker environment for backend and MySQL. Add initial JPA models for Program and Review.

---
+++ MadAbroad-Backend/src/main/java/com/patrickwang/MadAbroad/MadAbroadApplication.java
@@ -0,0 +1,14 @@
+package com.patrickwang.MadAbroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadAbroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadAbroadApplication.class, args);
+	}
+
+}
+
---
+++ MadAbroad-Backend/src/test/java/com/patrickwang/MadAbroad/MadAbroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrickwang.MadAbroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadAbroadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
