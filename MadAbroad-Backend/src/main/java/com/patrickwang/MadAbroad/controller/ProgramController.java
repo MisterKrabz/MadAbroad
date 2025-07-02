f6c7979b797149dfbdf4a503acfc3316fe8521f8
Patrick Wang <patrickwang@Patricks-MacBook-Pro-3.local>
2025-07-02 14:27:24 -0500

    adjusted review section star ratings to now have Social Scene, academic difficulty, credit transferability, and amount of culture shock and removed the generic difficulty category

--- MadAbroad-Backend/src/main/java/com/patrickwang/MadAbroad/controller/ProgramController.java
+++ MadAbroad-Backend/src/main/java/com/patrickwang/MadAbroad/controller/ProgramController.java
@@ -3,11 +3,15 @@ package com.patrickwang.MadAbroad.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.patrickwang.MadAbroad.model.StudyAbroadProgram;
@@ -52,5 +56,20 @@ public class ProgramController {
     @GetMapping("/search")
     public List<StudyAbroadProgram> searchPrograms(@RequestParam("q") String query) {
         return programService.searchPrograms(query);
+    }
+
+    /**
+     * This method creates a new program. Used by the web scraper for initial data population and by admin for 
+     * manual data entry in the event of a new program being added 
+     * 
+     * @param program
+     * @return
+     */
+    @PostMapping // This annotation maps HTTP POST requests for /api/programs to this method.
+    @ResponseStatus(HttpStatus.CREATED) // Sets the HTTP response status to 201 Created on success.
+    public StudyAbroadProgram createProgram(@RequestBody StudyAbroadProgram program) {
+        // The @RequestBody annotation tells Spring to convert the JSON from the request body
+        // into a StudyAbroadProgram object.
+        return programService.saveProgram(program); // You'll need to add a 'saveProgram' method to your service.
     }
 }
